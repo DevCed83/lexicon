108a6af46a6d4f04520759bb8942db2bce5dbb9d
FunkyChaper <cedric.montanuy@gmail.com>
2020-04-30 12:28:21 +0200

    ajout details fonctionnel
    Use case, user story, IHM

---
+++ functional_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+class Lexicon_functional_test(unittest.TestCase):
+	"""docstring for Lexicon_functional_test"""
+	def setUp(self):
+		pass
+	def tearDown(self):
+		pass
+	def test_GUI(self):
+		
+
+if __name__ == "__main__":
+	unittest.main()
+
